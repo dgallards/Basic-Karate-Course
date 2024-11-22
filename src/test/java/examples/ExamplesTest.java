ad6be626e573ceef754a07cf7fce3cdb9148571c
Diego <dgallards@alumnos.unex.es>
2024-11-22 05:39:43 +0100

    ultimate changes

--- src/test/java/examples/ExamplesTest.java
+++ src/test/java/examples/ExamplesTest.java
@@ -9,7 +9,7 @@ class ExamplesTest {
 
     @Test
     void testParallel() {
-        Results results = Runner.path("classpath:examples/myTests/exampleWEB.feature")
+        Results results = Runner.path("classpath:examples/myTests")
                 //.outputCucumberJson(true)
                 .parallel(5);
         assertEquals(0, results.getFailCount(), results.getErrorMessages());
